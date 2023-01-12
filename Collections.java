87b09eaa0c8d2434f8095776fb7c86efbfc65045
Mikiyas Girma <mikumaal223@gmail.com>
2023-01-12 04:21:40 -0800

    Update Collections.java

--- Collections.java
+++ Collections.java
@@ -8,30 +8,35 @@ public class NewMain {
     public static void main(String[] args) {
 LinkedList<String> cars = new LinkedList<String>();
 ArrayList<String> list=new ArrayList<String>();
+
+    studs.add("Mike");
+    studs.add("Bini");
+    studs.add("Yordi");
+    studs.add("Sam");
+         
+    list.add("2193");
+    list.add("2332");
+    list.add("1212");
+    list.add("3632");
+    
+      System.out.println(cars); 
+    System.out.println(list); 
+    
+      
 Map<String, Integer> countrycode=new HashMap<String ,Integer>();
-countrycode.put("ETS", 3523);
-countrycode.put("MTN", 2233);
-countrycode.put("ETC", 2211);
-countrycode.put("ETS", 3523);
+    countrycode.put("ETS", 3523);
+    countrycode.put("USA", 2233);
+    countrycode.put("UAI", 2211);
+    countrycode.put("ETS", 3523);
 
 System.out.println(countrycode.keySet());
      System.out.println(countrycode.values());
 
 
-cars.add("Volvo");
-    cars.add("BMW");
-    cars.add("Ford");
-    cars.add("Mazda");
-    
-    list.add("2000");
-    list.add("1937");
-    list.add("New model");
-    list.add("2023");
+
 
     
     
-    System.out.println(cars); 
-    System.out.println(list); 
         
         
         
